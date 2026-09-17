52f995d81f0eee5b961503c475eb37449dececf2
ankita chowdhury <ankitachowdhury145@gmail.com>
2026-09-17 12:44:42 +0530

    Create Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-
 import Header from './components/Common/Header'
-import Footer from './components/Common/Footer'
 import Home from './Pages/Home'
-import Contact from './Pages/Contact'
-import Shop from './Pages/Shop'
+import Footer from './components/Common/Footer'
 
 function App() {
 
-
   return (
     <div>
+
       <Header />
+
       <Home />
-      <Shop/>
-      <Contact />
+
+
+
+
+
+      
+
       <Footer />
+
     </div>
   )
 }
--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,13 +1,76 @@
+import React from "react";
+import "./Footer.css";
 
-import React from 'react'
-
-const Footer
- = () => {
+const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className="footer">
 
-export default Footer
+      <div className="footer-container">
+
+        <div className="footer-company">
+
+          <h2>Furniro.</h2>
+
+          <p>
+            400 University Drive Suite 200 Coral Gables,
+            <br />
+            FL 33134 USA
+          </p>
+
+        </div>
+
+
+      
+        <div className="footer-column">
+
+          <h3>Links</h3>
+
+          <a href="/">Home</a>
+          <a href="/shop">Shop</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+
+        </div>
+
+
+        <div className="footer-column">
+
+          <h3>Help</h3>
+
+          <a href="/payment">Payment Options</a>
+          <a href="/returns">Returns</a>
+          <a href="/privacy">Privacy Policies</a>
+
+        </div>
+ 
+        <div className="footer-newsletter">
+
+          <h3>Newsletter</h3>
+
+          <div className="subscribe-box">
+
+            <input
+              type="email"
+              placeholder="Enter Your Email Address"
+            />
+
+            <button>SUBSCRIBE</button>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+      <div className="footer-bottom">
+
+        <p>2023 furniro. All rights reserved</p>
+
+      </div>
+
+    </footer>
+  );
+};
+
+export default Footer;
